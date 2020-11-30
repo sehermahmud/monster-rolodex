e45fcc18709ccec21cf8ecf47d0f5bfbb64f93c7
sehermahmud <seher.mahmud@gmail.com>
2020-11-30 11:27:11 +0600

    Updates

--- src/App.js
+++ src/App.js
@@ -43,3 +43,39 @@ class App extends Component {
 }
 
 export default App;
+
+{
+  /*
+  seher - The snow glows white on the mountain tonight
+  Not a footprint to be seen
+  A kingdom of isolation
+  And it looks like
+  I'm the queen
+
+  seher - The wind is howling like this swirling storm inside
+  Couldn't keep it in
+  Heaven knows I tried
+
+  seher - Don't let them in
+  Don't let them see
+  Be the good girl you always have to be
+  Conceal, don't feel
+  Don't let them know
+  Well, now they know
+
+  safin - Let it go
+  Let it go
+
+  seher - Can't hold it back anymore
+
+  safin - Let it go
+  Let it go
+
+  seher - Turn away and slam the door
+
+  safin - I don't care
+  What they're going to say
+  Let the storm rage on
+  The cold never bothered me anyway
+*/
+}
